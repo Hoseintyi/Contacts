5018ddf1e02b05c64d7679914915935c22d69f13
Hosein Teymouri <Hoseint629@gmail.com>
2021-12-04 08:25:29 +0330

    Initial Commit
    in avalin bar estafadas taze porozhe sakhte shude badanash va hich ignori dar nazar gerfte nashude

---
+++ Repository/IContactRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace Contacts.Repository
+{
+    interface IContactRepository
+    {
+        bool Insert(string name, string family, string mobile , string staticPhone , string email);
+
+        bool Update(int contactId,string name, string family, string mobile, string staticPhone, string email);
+
+        bool Delete(int contactId);
+
+        DataTable SelectAll();
+
+        DataTable SelectContactById(int contactId);
+
+
+    }
+}
---
+++ Services/ContactRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using Contacts.Repository;
+using System.Data.SqlClient;
+
+namespace Contacts.Services
+{
+  public  class ContactRepository:IContactRepository
+    {
+        private string myconnection = "DataSource=.; Initial Catalog= CotactWindowsFormApplication_DB; Integrated Security= true";
+        public bool Insert(string name, string family, string mobile, string staticPhone, string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(int contactId, string name, string family, string mobile, string staticPhone, string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int contactId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataTable SelectAll()
+        {
+            string query = "Select * From contactInfo";
+            SqlConnection connection = new SqlConnection(myconnection);
+            SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+            DataTable data = new DataTable();
+            adapter.Fill(data);
+            return data;
+        }
+
+        public DataTable SelectContactById(int contactId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
